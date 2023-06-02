02d9d0b78733075b4317bc0ef64effb0328ae078
Devan Goodwin <dgoodwin@redhat.com>
2023-06-02 09:00:37 -0300

    Break GCS intervals impl out into it's own func

--- pkg/api/jobrunintervals/job_run_intervals.go
+++ pkg/api/jobrunintervals/job_run_intervals.go
@@ -10,6 +10,7 @@ import (
 	"github.com/openshift/sippy/pkg/api"
 	apitype "github.com/openshift/sippy/pkg/apis/api"
 	"github.com/openshift/sippy/pkg/db"
+	"github.com/openshift/sippy/pkg/db/models"
 	"github.com/openshift/sippy/pkg/prowloader/gcs"
 	log "github.com/sirupsen/logrus"
 )
@@ -17,16 +18,21 @@ import (
 // JobRunIntervals fetches intervals for a given job run.
 func JobRunIntervals(gcsClient *storage.Client, dbc *db.DB, jobRunID int64, logger *log.Entry) (*apitype.EventIntervalList, error) {
 
-	// Right now, we need the job run in our DB to fetch its URL, so we can find the GCS
-	// bucket. This means until sippy imports the job, you will not be able to fetch
+	// Right now, we need the job run in our DB to fetch its URL or job name, so we can find the
+	// appropriate logs in GCS or loki. This means until sippy imports the job, you will not be able to fetch
 	// its intervals.
-	// However, long term we expect these to live in an external system, and the prow job ID
-	// should be all we need to look it up. At that point the limitation should be removed.
+	// TODO: force specifying the job name as well in api params, so we don't need to do this if using loki
 	jobRun, _, err := api.FetchJobRun(dbc, jobRunID, logger)
 	if err != nil {
 		logger.WithError(err).Error("error querying job run")
 		return nil, err
 	}
+
+	return fetchIntervalsFromGCS(gcsClient, jobRun, logger)
+}
+
+func fetchIntervalsFromGCS(gcsClient *storage.Client, jobRun *models.ProwJobRun, logger *log.Entry) (*apitype.EventIntervalList, error) {
+	intervals := &apitype.EventIntervalList{}
 
 	parts := strings.Split(jobRun.URL, gcs.OpenshiftGCSBucket)
 	path := parts[1][1:]
@@ -39,7 +45,6 @@ func JobRunIntervals(gcsClient *storage.Client, dbc *db.DB, jobRunID int64, logg
 	// We will often match multiple files here, one for upgrade phase, one for conformance
 	// testing phase. For now, we return them all, and each interval has a filename it
 	// originated from.
-	intervals := &apitype.EventIntervalList{}
 	if len(intervalFiles) == 0 {
 		logger.Info("no interval files found")
 		return intervals, nil
@@ -67,6 +72,5 @@ func JobRunIntervals(gcsClient *storage.Client, dbc *db.DB, jobRunID int64, logg
 
 		intervals.Items = append(intervals.Items, newIntervals.Items...)
 	}
-
 	return intervals, nil
 }
