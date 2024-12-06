d7696fe32ad73093a3452a20293cedec6f324601
Devan Goodwin <dgoodwin@redhat.com>
2024-12-06 13:39:36 -0400

    Implement cache cleanup routine

--- pkg/cache/disk/disk.go
+++ pkg/cache/disk/disk.go
@@ -7,10 +7,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/gofrs/flock"
 	"github.com/sirupsen/logrus"
+)
+
+const (
+	cacheExpiryScanInterval = 1 * time.Minute
 )
 
 type Cache struct {
@@ -18,9 +23,12 @@ type Cache struct {
 }
 
 func NewDiskCache() (*Cache, error) {
-	return &Cache{
+	// Start the cleanup goroutine
+	cache := &Cache{
 		cacheDir: "/tmp/sippycache",
-	}, nil
+	}
+	go cache.cleanupExpiredCacheFiles()
+	return cache, nil
 }
 
 // GenKeyFilename generates a SHA-256 hash of the input string, and uses it to return a filename
@@ -47,7 +55,15 @@ func (c Cache) Get(_ context.Context, key string, _ time.Duration) ([]byte, erro
 	if err != nil {
 		return nil, fmt.Errorf("error reading cache file %s", filename)
 	}
+	logrus.Infof("returned cache file data from %s", filename)
 	return data, nil
+}
+
+type cacheKeyFileMetadata struct {
+	Key      string    `json:"key"`
+	Filename string    `json:"filename"`
+	Set      time.Time `json:"set"`
+	Expire   time.Time `json:"expire"`
 }
 
 func (c Cache) Set(_ context.Context, key string, content []byte, duration time.Duration) error {
@@ -67,11 +83,13 @@ func (c Cache) Set(_ context.Context, key string, content []byte, duration time.
 	}
 
 	// Create metadata
-	metadata := map[string]interface{}{
-		"key":      key,
-		"filename": filename,
-		"set":      before.Format(time.RFC3339),
-		"expire":   before.Add(duration).Format(time.RFC3339),
+	metadata := cacheKeyFileMetadata{
+		Key:      key,
+		Filename: filename,
+		//set:      before.Format(time.RFC3339),
+		//expire:   before.Add(duration).Format(time.RFC3339),
+		Set:    before,
+		Expire: before, // TODO: add duration
 	}
 
 	// metadata filename, this will contain details on the original key, set time and expiry time
@@ -88,7 +106,6 @@ func (c Cache) Set(_ context.Context, key string, content []byte, duration time.
 	}
 	defer lock.Unlock()
 
-	// Write metadata to file
 	metadataContent, err := json.MarshalIndent(metadata, "", "  ")
 	if err != nil {
 		return fmt.Errorf("failed to marshal metadata: %v", err)
@@ -104,4 +121,76 @@ func (c Cache) Set(_ context.Context, key string, content []byte, duration time.
 		return fmt.Errorf("failed to write to cache file: %v", err)
 	}
 	return nil
+}
+
+// cleanupExpiredCacheFiles periodically checks all metadata files, and deletes expired cache files and metadata if
+// we're past their expiration time.
+func (c *Cache) cleanupExpiredCacheFiles() {
+	ticker := time.NewTicker(cacheExpiryScanInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			c.cleanup()
+		}
+	}
+}
+
+// cleanup performs the actual cleanup of expired files. Uses the same file lock to prevent concurrent writes with
+// Set or another sippy replica.
+func (c *Cache) cleanup() {
+	files, err := os.ReadDir(c.cacheDir)
+	if err != nil {
+		logrus.Errorf("failed to read cache directory %s: %v", c.cacheDir, err)
+		return
+	}
+
+	for _, file := range files {
+		if filepath.Ext(file.Name()) == "-metadata.json" {
+			// read and parse metadata file to see if we're past our expiration time:
+			metadataFilename := filepath.Join(c.cacheDir, file.Name())
+			metadataContent, err := os.ReadFile(metadataFilename)
+			if err != nil {
+				logrus.Errorf("failed to read metadata file %s: %v", metadataFilename, err)
+				continue
+			}
+
+			var metadata cacheKeyFileMetadata
+			if err := json.Unmarshal(metadataContent, &metadata); err != nil {
+				logrus.Errorf("failed to unmarshal metadata from file %s: %v", metadataFilename, err)
+				continue
+			}
+
+			// if expired, delete cache file and metadata file
+			if time.Now().After(metadata.Expire) {
+				cacheFile := metadata.Filename
+
+				// we just lock on the actual cache data file on the assumption the metadata file is only
+				// ever written when the data file is locked.
+				cacheLock := flock.New(cacheFile + ".lock")
+
+				cacheLocked, err := cacheLock.TryLock()
+				if err != nil {
+					logrus.Errorf("failed to lock cache file %s: %v", cacheFile, err)
+					continue
+				}
+				if !cacheLocked {
+					logrus.Infof("cache file %s is locked by another process, skipping cleanup.", cacheFile)
+					continue
+				}
+				defer cacheLock.Unlock()
+
+				logrus.Infof("deleting expired cache file %s and its metadata", cacheFile)
+
+				// remove cache file first, then the metadata
+				if err := os.Remove(cacheFile); err != nil {
+					logrus.Errorf("failed to delete expired cache file %s: %v", cacheFile, err)
+				}
+				if err := os.Remove(metadataFilename); err != nil {
+					logrus.Errorf("failed to delete expired metadata file %s: %v", metadataFilename, err)
+				}
+			}
+		}
+	}
 }
